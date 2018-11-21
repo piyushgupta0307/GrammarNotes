cc5bf4d4a94eb2678a6ef363e0ac90654c97ab89
Piyush Kumar Gupta <Piyush.9.Gupta@in.niit.com>
2018-11-21 18:14:05 +0530

    pagination modified by piyush

--- src/app/grammar-note/grammar-note.component.ts
+++ src/app/grammar-note/grammar-note.component.ts
@@ -2,7 +2,7 @@ import { ViewChild, Pipe, Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpErrorResponse } from '@angular/common/http';
 import { NgxPaginationModule } from 'ngx-pagination';
-import {DomSanitizer} from "@angular/platform-browser";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-grammar-note',
@@ -20,16 +20,17 @@ export class GrammarNoteComponent {
   sign = 'plus';  // or minus if you want that first
   sign1 = 'plus';
   arrImage: string[];
-  
+
   typeOfDownload: any = "";
   downloadIndex = 0;
   private assestsPath = "../../assets/";
   public videoPath: any;
-  
-  constructor(private httpService: HttpClient, private domSanitizer : DomSanitizer) { }
+  public htmlPath: any;
+  private indexOfElement = 0;
+  constructor(private httpService: HttpClient, private domSanitizer: DomSanitizer) { }
 
   ngOnInit() {
-    this.httpService.get(this.assestsPath+'/data/data.json').subscribe(
+    this.httpService.get(this.assestsPath + '/data/data.json').subscribe(
       data => {
         this.arrImage = data as string[];	 // FILL THE ARRAY WITH DATA.
         console.log(this.arrImage[1]);
@@ -38,8 +39,9 @@ export class GrammarNoteComponent {
         console.log(err.message);
       }
     );
+    this.htmlPath = this.domSanitizer.bypassSecurityTrustResourceUrl(this.assestsPath + "images/image 78.png");
   }
-  
+
   toggleSign() {
     if (this.sign == 'plus') {
       this.sign = 'minus';
@@ -61,18 +63,20 @@ export class GrammarNoteComponent {
   }
 
   private playVideo(videoPat, inex) {
+    this.htmlPath = "";
     // this.videoPath= this.assestsPath + 'video/level2_lesson01_v2/index.html';
     this.downloadIndex = inex;
     this.videoPath = this.assestsPath + videoPat;
-    this.domSanitizer.bypassSecurityTrustResourceUrl(this.videoPath);
+    this.htmlPath = this.domSanitizer.bypassSecurityTrustResourceUrl(this.videoPath);
     // this.playVideoAuto();
   }
 
   // playVideoAuto() {
   //   this.videoplayer.nativeElement.play();
   // }
+
   download() {
     let file = this.arrImage[this.downloadIndex][this.typeOfDownload];
-    window.open(this.assestsPath+file);
+    window.open(this.assestsPath + file);
   }
 }
